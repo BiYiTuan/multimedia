6a922cd60a163b4e2773d24fad5f2f62ba97f6db
Hong Yu <jekyll.hong@hotmail.com>
2018-01-25 11:31:12 +0800

    增加了MessageType模块，包含各种message的定义

---
+++ src/main/java/com/multimedia/message/MessageType.java
@@ -0,0 +1,14 @@
+package com.multimedia.message;
+
+public final class MessageType {
+    private static final int MSG_MEDIA_SOURCE = 100;
+
+    /**
+     * messages in MediaSource
+     */
+    public static final int MEDIA_SOURCE_LOAD = MSG_MEDIA_SOURCE + 1;
+
+    private MessageType() {
+        //nothing
+    }
+}
