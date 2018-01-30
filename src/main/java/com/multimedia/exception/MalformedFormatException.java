ba6fc98238e4adbbdb68d25c766a46cf832b7659
Hong Yu <jekyll.hong@hotmail.com>
2018-01-30 13:13:49 +0800

    新增MalformedFormatException，表示媒体格式解析时的异常

---
+++ src/main/java/com/multimedia/exception/MalformedFormatException.java
@@ -0,0 +1,11 @@
+package com.multimedia.exception;
+
+public class MalformedFormatException extends Exception {
+    public MalformedFormatException() {
+        super();
+    }
+
+    public MalformedFormatException(String detail) {
+        super(detail);
+    }
+}
